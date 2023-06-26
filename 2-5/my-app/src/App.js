21431acce20a1ddb88684621982de784da265c07
chaeeunj <official2930@naver.com>
2023-06-27 03:43:19 +0900

    리액트 props 사용해보기

--- 2-5/my-app/src/App.js
+++ 2-5/my-app/src/App.js
@@ -4,7 +4,8 @@ import Header from './components/Header.js';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title={'Learn React A'} />
+      <Header title={'Learn React B'} />
     </div>
   );
 }
--- 2-5/my-app/src/components/Header.js
+++ 2-5/my-app/src/components/Header.js
@@ -1,25 +1,9 @@
 import React from 'react';
 
-const Header = () => {
-  // const getGreetingMessage = function (name) {
-  //   if (name === 'jce') return `Hello, ${name}! Learn React`;
-  //   return `Welcome, ${name}! Learn React`;
-  // };
-
+const Header = (props) => {
   const name = 'jce';
-  const count = 0;
-  const memo = '';
 
-  return (
-    <header className="App-header">
-      {name === 'jce'
-        ? `Hello, ${name}! Learn React`
-        : `Welcome, ${name}! Learn React`}
-
-      <div>{count && <h1>Messages: {count}</h1>}</div>
-      {memo && `메모: ${memo}`}
-    </header>
-  );
+  return <header className="App-header">{props.title}</header>;
 };
 
 export default Header;
