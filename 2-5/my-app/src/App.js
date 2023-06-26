0c1f053fd33293bdd87ee536e543a74b435bc043
chaeeunj <official2930@naver.com>
2023-06-27 03:24:36 +0900

    조건부 렌더링

--- 2-5/my-app/src/App.js
+++ 2-5/my-app/src/App.js
@@ -1,32 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
-  const message = 'Learn React';
-
-  const getGreetingMessage = (name) => {
-    if (name === 'jce') return `Hello, ${name}! Learn React`;
-    return `Welcome, ${name}! Learn React`;
-  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          {getGreetingMessage('ad')}
-        </a>
-      </header>
-      <input type="checkbox" name="agreement" id="agreement-el"></input>
-      <lavel htmlFor="agreement-el">동의</lavel>
+      <Header />
     </div>
   );
 }
-
 export default App;
