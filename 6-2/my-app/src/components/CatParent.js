97d61862778f90d5693ebeee3a14649dba7b9f24
chaeeunj <official2930@naver.com>
2023-06-28 18:06:40 +0900

    ref 심화 - callback ref

--- 6-2/my-app/src/components/CatParent.js
+++ 6-2/my-app/src/components/CatParent.js
@@ -2,13 +2,17 @@ import React, { useRef, useState } from 'react';
 import Cat from './Cat';
 
 export default function CatParent() {
-  const catRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  const catCallbackRef = (node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  };
 
   console.log('부모 컴포넌트 CatParent');
-  console.log(catRef);
 
   const ageRef = useRef(1);
-  const [state, setState] = useState(1);
 
   console.log(ageRef.current);
 
@@ -16,16 +20,12 @@ export default function CatParent() {
     <div>
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
-        <Cat ref={catRef} />
+        <Cat ref={catCallbackRef} />
         <h3>나이 : {ageRef.current}살</h3>
-        <h4>상태값 : {state}</h4>
-        <button onClick={() => alert(catRef.current.height)}>
-          이미지의 사이즈를 알고싶어
-        </button>
+        <h4>키 : {height}</h4>
         <button onClick={() => (ageRef.current = ageRef.current + 1)}>
           새해
         </button>
-        <button onClick={() => setState(state + 1)}>테스트</button>
       </div>
     </div>
   );
