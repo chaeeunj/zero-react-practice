17d45bf3fb3cdfc2865837176928957e474cf31b
chaeeunj <official2930@naver.com>
2023-06-28 14:06:41 +0900

    비밀번호 추가

--- 6-2/my-app/src/components/SimpleForm.js
+++ 6-2/my-app/src/components/SimpleForm.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 export default function SimpleForm() {
   const [nickname, setNickName] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleChange = (e) => {
-    setNickName(e.target.value);
+    if (e.target.name === 'nickname') return setNickName(e.target.value);
+    return setPassword(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(nickname);
+    alert(`nickname : ${nickname}, password : ${password}`);
   };
 
   return (
@@ -20,6 +22,14 @@ export default function SimpleForm() {
         name="nickname"
         onChange={handleChange}
         value={nickname}
+      />
+      <br />
+      <label>패스워드 :</label>
+      <input
+        type="text"
+        name="password"
+        onChange={handleChange}
+        value={password}
       />
       <input type="submit" value="제출" />
     </form>
